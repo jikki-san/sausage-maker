5dbb50611468901649e204635323d02417540556
Jackson Eckert <jaxon.eckert@gmail.com>
2025-05-24 02:05:14 -0400

    Allow flexible basepath for GH Pages deploy

--- src/gen_pages.py
+++ src/gen_pages.py
@@ -4,24 +4,24 @@ from block_markdown import extract_title
 from markdown_to_html import markdown_to_htmlnode
 
 
-def generate_pages_recursive(content_path, template_path, dest_path):
+def generate_pages_recursive(base_path, content_path, template_path, dest_path):
     if os.path.isfile(content_path):
         _, file_name = os.path.split(content_path)
         file_name, ext = file_name.split(".")
         if ext != "md":
             raise ValueError("improper file format")
         dest_path, _ = os.path.split(dest_path)
-        generate_page(content_path, template_path,
+        generate_page(base_path, content_path, template_path,
                       os.path.join(dest_path, f"{file_name}.html"))
         return
     for item in os.listdir(content_path):
         new_content_path = os.path.join(content_path, item)
         new_dest_path = os.path.join(dest_path, item)
         generate_pages_recursive(
-            new_content_path, template_path, new_dest_path)
+            base_path, new_content_path, template_path, new_dest_path)
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(base_path, from_path, template_path, dest_path):
     print(
         f"Generating page {dest_path} from {from_path} using {template_path}")
     markdown_file = open(from_path, "r")
@@ -36,6 +36,8 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", content.to_html())
+    template = template.replace('href="/', f'href="{base_path}')
+    template = template.replace('src="/', f'src="{base_path}')
 
     dest_path_dir = os.path.dirname(dest_path)
     if dest_path_dir != "":
--- src/main.py
+++ src/main.py
@@ -1,14 +1,24 @@
+import sys
+
+
 from gen_pages import generate_pages_recursive
 from util import dir_copy
 
 
-SOURCE = "static"
-DEST = "public"
+STATIC_SOURCE = "static"
+CONTENT_SOURCE = "content"
+DEST = "docs"
+TEMPLATE_FILE = "template.html"
 
 
 def main():
-    dir_copy(SOURCE, DEST)
-    generate_pages_recursive("content", "template.html", "public")
+    base_path = "/"
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+
+    dir_copy(STATIC_SOURCE, DEST)
+    generate_pages_recursive(base_path, CONTENT_SOURCE,
+                             TEMPLATE_FILE, DEST)
 
 
 if __name__ == "__main__":
